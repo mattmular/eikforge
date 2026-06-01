85cf19c6f835b74a172e17bcc97fbbdfbe4e0355
mitsukeni <mattmular@gmail.com>
2026-06-01 14:09:31 -0600

    added set-ip command and cogs

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import sys
 import os
 from dotenv import load_dotenv
+import json
 import subprocess
-import sys
 
 import discord
 from discord import app_commands
@@ -13,29 +14,20 @@ load_dotenv()
 MY_GUILD = discord.Object(id=os.getenv("GUILD_ID"))
 ADMIN_ROLES = [role.strip() for role in os.getenv("ADMIN_ROLES").split(",")]
 
+with open("private.json", "r") as file:
+    private = json.load(file)
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
-    
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
     try:
         synced = await client.tree.sync(guild=MY_GUILD)
         print(f"Synced {len(synced)} command(s) to guild {MY_GUILD.id}")
     except Exception as e:
         print(f"Error syncing commands: {e}")
-
-@client.tree.command(name="wiki", description="information", guild=MY_GUILD)
-async def wiki(interaction: discord.Interaction):
-    embed = discord.Embed(
-        title="Title",
-        description="Descriptions",
-        color=discord.Colour.blue()
-        )
-    embed.add_field(name="Field 1", value="Hi", inline=True)
-    embed.set_footer(text="ID: " + str(interaction.user.id))
-
-    view = PersistentView()
-
-    await interaction.response.send_message(embed=embed, view=view)
 
 @client.tree.command(name="gitpull", description="Pulls the latest commit from the git repository", guild=MY_GUILD)
 @app_commands.checks.has_any_role(*ADMIN_ROLES)
@@ -53,13 +45,20 @@ async def restart(interaction: discord.Interaction):
 @client.tree.command(name="refresh", description="Refreshes embeds", guild=MY_GUILD)
 @app_commands.checks.has_any_role(*ADMIN_ROLES)
 async def refresh(interaction: discord.Interaction):
-    await interaction.response.send_message("Embeds have been refreshed")
+    try:
+        await client.reload_extension("cogs.embeds")
+        await interaction.response.send_message("Embeds have been refreshed", ephemeral=True)
+    except Exception as e:
+        await interaction.response.send_message(f"Failed to reload embeds.\nError: `{e}`", ephemeral=True) 
+
+
 
 @client.tree.error
 async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
 
     if isinstance(error, (app_commands.MissingRole, app_commands.MissingAnyRole)):
-
+        print(error)
+        print(interaction.user.roles)
         await interaction.response.send_message(
             "insufficient permissions", 
             ephemeral=True
@@ -76,26 +75,7 @@ async def on_app_command_error(interaction: discord.Interaction, error: app_comm
 
     # Print any other unexpected bugs to your terminal so you can debug them
     print(f"Sorry we ran into an error processing your command: {error}")
-
-class PersistentView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-
-    @discord.ui.button(label="Rules", style=discord.ButtonStyle.primary)
-    async def rules_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        rules_embed = discord.Embed(title="Rules", color=discord.Colour.red())
-        rules_embed.add_field(name="rule 1", value="ayo be chill")
-
-        await interaction.response.send_message(embed=rules_embed, ephemeral=True)
-
-    @discord.ui.button(label="How to Join", style=discord.ButtonStyle.primary)
-    async def joinInstructions_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        join_embed = discord.Embed(title="How to Join", color=discord.Colour.red())
-        join_embed.add_field(name="Server IP:", value="doed.dat.airforce:20134")
-        join_embed.add_field(name="Password:", value="beeefcake")
-
-        await interaction.response.send_message(embed=join_embed, ephemeral=True)
-
+    
 async def git_pull():
     try:
         result = subprocess.run(["git","pull","origin","main"], check=True,capture_output=True,text=True)
---
+++ cogs/embeds.py
@@ -0,0 +1,68 @@
+
+import os
+from dotenv import load_dotenv
+import json
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+load_dotenv()
+MY_GUILD = discord.Object(id=os.getenv("GUILD_ID"))
+ADMIN_ROLES = [int(role.strip()) for role in os.getenv("ADMIN_ROLES").split(",")]
+
+with open("private.json", "r") as file:
+    private = json.load(file)
+
+class Embeds(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+
+    @app_commands.command(name="set-ip", description="Sets the server IP")
+    @app_commands.guilds(MY_GUILD)
+    @app_commands.checks.has_any_role(*ADMIN_ROLES)
+    async def setip(self, interaction: discord.Interaction, ip: str):
+        private["ip"] = ip
+        with open("private.json","w") as file:
+            json.dump(private, file)
+        await interaction.response.send_message(f"Set the server IP to {ip}")
+
+    @app_commands.command(name="wiki", description="information")
+    @app_commands.guilds(MY_GUILD)
+    async def wiki(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="Title",
+            description="Descriptions",
+            color=discord.Colour.blue()
+            )
+        embed.add_field(name="Field 1", value="Hi", inline=True)
+        embed.set_footer(text="ID: " + str(interaction.user.id))
+
+        view = PersistentView()
+
+        await interaction.response.send_message(embed=embed, view=view)
+
+class PersistentView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Rules", style=discord.ButtonStyle.primary)
+    async def rules_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        rules_embed = discord.Embed(title="Rules", color=discord.Colour.red())
+        rules_embed.add_field(name="rule 1", value="ayo be chill")
+
+        await interaction.response.send_message(embed=rules_embed, ephemeral=True)
+
+    @discord.ui.button(label="How to Join", style=discord.ButtonStyle.primary)
+    async def joinInstructions_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        join_embed = discord.Embed(title="How to Join", color=discord.Colour.red())
+        join_embed.add_field(name="Server IP:", value=private["ip"])
+        join_embed.add_field(name="Password:", value="beeefcake")
+
+        await interaction.response.send_message(embed=join_embed, ephemeral=True)
+
+
+
+
+async def setup(client):
+    await client.add_cog(Embeds(client))
