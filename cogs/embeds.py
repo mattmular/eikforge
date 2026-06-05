60557c10bffb28da93e51d28d5f429f3a8ebef7a
mitsukeni <mattmular@gmail.com>
2026-06-05 04:07:45 -0600

    r2 command

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -18,6 +18,10 @@ JoinDetails = discord.Embed(title="Join details", description= "Please avoid sha
 JoinDetails.add_field(name="Server IP:", value=private["ip"], inline=False)
 JoinDetails.add_field(name="Password:", value=private["password"], inline=False)
 
+r2Setup = discord.Embed(title="Starter Pack", description="We'll help you get set up with a starter mod pack of curated mods that we recommend.", color=discord.Colour(0x0785cb))
+r2Setup.set_thumbnail(url="https://thunderstore.io/thumbnail-serve/repository/icons/ebkr-r2modman-3.2.17.png/?width=256&height=256")
+r2Setup.add_field(name="STEPS", value="1. Install [r2modman](https://thunderstore.io/package/ebkr/r2modman/) from the Thunderstore by clicking **Manual Download**.\n2. Run the application and select Valheim.\n3. In the Profile selection screen click **Import / Update.**\n4. Select **From code**.\n5. Paste this code `019e952c-d4b9-e2b5-7544-1d7c43dd9415` into the text box and select **Continue** then **Import**.\n6. Click on the profile and then **Select profile**.\n7. In the top left click **Start modded** to run Valheim with the mods installed.", inline=False)
+
 class Embeds(commands.Cog):
     def __init__(self,client):
         self.client = client
@@ -64,6 +68,11 @@ class Embeds(commands.Cog):
     @app_commands.guilds(MY_GUILD)
     async def join(self, interaction: discord.Interaction):
         await interaction.response.send_message(embed=JoinDetails)
+
+    @app_commands.command(name="r2modman", description="r2modman setup")
+    @app_commands.guilds(MY_GUILD)
+    async def join(self, interaction: discord.Interaction):
+        await interaction.response.send_message(embed=r2Setup)
     
     @app_commands.command(name="screenshotwebhook", description="Webhooks for screenshot uploads")
     @app_commands.guilds(MY_GUILD)
@@ -148,10 +157,7 @@ class ModsView(discord.ui.View):
     
     @discord.ui.button(label="r2modman Setup", style=discord.ButtonStyle.primary, custom_id=f"{MY_GUILD}:mods_r2modman")
     async def r2modman_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        embed = discord.Embed(title="Starter Pack", description="We'll help you get set up with a starter mod pack of curated mods that we recommend.", color=discord.Colour(0x0785cb))
-        embed.set_thumbnail(url="https://thunderstore.io/thumbnail-serve/repository/icons/ebkr-r2modman-3.2.17.png/?width=256&height=256")
-        embed.add_field(name="STEPS", value="1. Install [r2modman](https://thunderstore.io/package/ebkr/r2modman/) from the Thunderstore by clicking **Manual Download**.\n2. Run the application and select Valheim.\n3. In the Profile selection screen click **Import / Update.**\n4. Select **From code**.\n5. Paste this code `019e952c-d4b9-e2b5-7544-1d7c43dd9415` into the text box and select **Continue** then **Import**.\n6. Click on the profile and then **Select profile**.\n7. In the top left click **Start modded** to run Valheim with the mods installed.", inline=False)
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.response.send_message(embed=r2Setup, ephemeral=True)
 
     #@discord.ui.button(label="FDI Setup", style=discord.ButtonStyle.primary)
     #async def fdi_button(self, interaction: discord.Interaction, button: discord.ui.Button):
